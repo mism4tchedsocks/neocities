5965d1114b7f683a024dc432fd2af7c8ce544239
Oliver Lecluyse <oliver.lecluyse@gmail.com>
2026-03-22 11:03:33 +0100

    22 MARCH 2026. had a duplicate of all the files in the main folder oopsies, deleted them

--- js/autoload.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $("header").load("/layout/header.html");
-  $("nav").load("/layout/nav.html");
-  $("footer").load("/layout/footer.html");
-  $("#latest").load("/layout/latest.html");
-  $("#now").load("/layout/now.html");
-});
