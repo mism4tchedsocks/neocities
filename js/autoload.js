86268f2a1e9fb485b1dcebf868568d937d58c429
Oliver Lecluyse <oliver.lecluyse@gmail.com>
2026-03-22 10:55:15 +0100

    21-22 MARCH 2026. big update/cleanup. still need to update the individual mmm pages and probably some other things as well

---
+++ js/autoload.js
@@ -0,0 +1,7 @@
+$(function(){
+  $("header").load("/layout/header.html");
+  $("nav").load("/layout/nav.html");
+  $("footer").load("/layout/footer.html");
+  $("#latest").load("/layout/latest.html");
+  $("#now").load("/layout/now.html");
+});
